d92c6faffefd7120fef3c393fa8cb447856885ed
Vibilan Jayanth <vibilan@firstindo.com>
2024-03-01 20:44:23 +0800

    changed sample image to png

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+import sys
 from process_image import read_image, compress_image, write_image
 from k_means import initialize_means, k_means
 
+# img = read_image(sys.argv[1])
+# clusters = sys.argv[2]
 
-# Read the rocks image
-img = read_image("images/rocks.jpg")
+# write_image("rocks_alt.png", img)
 
+img = read_image("rocks.png")
 clusters = 32
 
 points, centroids = initialize_means(img, clusters)
 centroids, index = k_means(points, centroids, clusters)
 compressed = compress_image(centroids, index, img)
 
-write_image("compressed.jpg", compressed)
+write_image("rocks_compressed.png", compressed)
